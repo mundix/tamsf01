127555ee1f24a517c90d0bddb61d4e24e0810f27
Edmundo Pichardo <epichardo@enteractiva.net>
2015-07-05 11:37:37 -0400

    Se crearon las migraciones de users, categories y candidates, y sus estructuras

--- app/database/migrations/2015_07_05_141933_create_users_table.php
+++ app/database/migrations/2015_07_05_141933_create_users_table.php
@@ -15,6 +15,13 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+
+            $table->string("full_name");
+            $table->string("email");
+            $table->string("password");
+            $table->enum("user_type",["admin","candidate"]);
+            $table->string("remember_token")->nullable(); //Function de Remember Me ,
+
 			$table->timestamps();
 		});
 	}
