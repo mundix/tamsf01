12c113c5e9874be9b291de130491d17bde23dcde
Edmundo Pichardo <epichardo@enteractiva.net>
2015-07-10 06:05:47 -0400

    Se crearon una parte de sisfactura de las mgiraciones

--- app/database/migrations/2015_07_10_100238_create_orders_status_table.php
+++ app/database/migrations/2015_07_10_100238_create_orders_status_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCategoriesTable extends Migration {
+class CreateOrdersStatusTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,11 +12,9 @@ class CreateCategoriesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('categories', function(Blueprint $table)
+		Schema::create('orders_status', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('slug');
 			$table->timestamps();
 		});
 	}
@@ -29,7 +27,7 @@ class CreateCategoriesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('categories');
+		Schema::drop('orders_status');
 	}
 
 }
